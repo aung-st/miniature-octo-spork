18f49ccb65e4b04986eb21a5514ed0919bad42b7
Sam Aung <92190177+aung-st@users.noreply.github.com>
2022-01-21 23:21:08 +0000

    Add files via upload

---
+++ CoolStatisticsStuff/standardDeviation.py
@@ -0,0 +1,20 @@
+from math import sqrt
+def standardDeviation(values):
+
+    mean = sum(values) / len(values)
+
+    #Get square of the difference between the value and mean for all values
+    diffSquared = 0
+    for i in range(len(values)):
+        diffSquared += (values[i] - mean) **2
+
+
+    return sqrt(diffSquared / (len(values)-1))
+
+
+if __name__ == "__main__":
+    sample1 = [124,124,265,346]
+    sample2 = [85,56,36,56,87,98,98,70,100,45,98,78,68,58,78]
+    print(standardDeviation(sample1))
+    print(standardDeviation(sample2))
+
