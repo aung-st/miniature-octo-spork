2a012ee1f0d189e1021a3e9cfc451a1d37f17c98
Sam Aung <92190177+aung-st@users.noreply.github.com>
2022-01-22 00:06:17 +0000

    Add files via upload

---
+++ MathsStuff/HCF.py
@@ -0,0 +1,15 @@
+def HCF(a,b):
+    if a > b:
+        bigger = a
+        smaller = b 
+    else:
+        bigger = b
+        smaller = a
+    
+    for factor in range(1,smaller-1): #the hcf won't be biggre than the smaller number
+        if a % factor == 0 and b % factor == 0:
+            hcf = factor
+    
+    return hcf
+
+print(HCF(198,360))
