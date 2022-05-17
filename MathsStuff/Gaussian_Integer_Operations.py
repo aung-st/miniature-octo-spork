7b6a247739bb422e102f92001d736bdbf363bed4
Sam Aung <92190177+aung-st@users.noreply.github.com>
2022-05-17 14:31:06 +0100

    Update Gaussian_Integer_Operations.py

--- MathsStuff/Gaussian_Integer_Operations.py
+++ MathsStuff/Gaussian_Integer_Operations.py
@@ -4,8 +4,6 @@ def divide_GI(a,b,c,d):
     f2_n = a * -d #R I -> I
     f3_n = b*c  # I R -> I
     f4_n = 0 #placeholder
-    if b <0 or d <0: #-i * i = -i^2 = 1
-        f4_n = b*d  #I I -> R 
     if b < 0 and d < 0: #-i * -i = i^2 = -1
         f4_n = b*-d
     else:
@@ -13,10 +11,9 @@ def divide_GI(a,b,c,d):
     fr_n = f1_n + f4_n 
     fi_n = f2_n + f3_n 
 
-    fr_d = c**2 
-    fi_d = d**2
+    denominator  = c**2 + d**2
     
-    print(f'{fr_n}/{fr_d+ fi_d} + {fi_n}i/{fr_d+ fi_d}') 
+    print(f'{fr_n}/{denominator } + {fi_n}i/{denominator }') 
     
 
     quotient_r = 0 #placeholder
@@ -24,26 +21,25 @@ def divide_GI(a,b,c,d):
     remainder_i = 0
     remainder_r = 0
     if fr_n < 0:
-        quotient_r = -fr_n // (fr_d+ fi_d)
-        remainder_r = -fr_n % (fr_d+ fi_d)
+        quotient_r = -fr_n // (denominator )
+        remainder_r = -fr_n % (denominator )
         remainder_r = -remainder_r
     else:
-        quotient_r = fr_n // (fr_d+ fi_d)
-        remainder_r = fr_n % (fr_d+ fi_d)
+        quotient_r = fr_n // (denominator )
+        remainder_r = fr_n % (denominator )
     if fi_n <0:
-        quotient_i = -fi_n // (fr_d+ fi_d)
-        remainder_i = -fi_n % (fr_d+ fi_d)
+        quotient_i = -fi_n // (denominator )
+        remainder_i = -fi_n % (denominator )
         remainder_i = -remainder_i
     else:
-        quotient_i = fi_n // (fr_d+ fi_d)
-        remainder_i = fi_n % (fr_d+ fi_d)
+        quotient_i = fi_n // (denominator )
+        remainder_i = fi_n % (denominator )
 
     
-    
    
-    print(f'quotient: {quotient_r} + {quotient_i}i')
-    print(f'remainder: {remainder_r}+{remainder_i}i')
+    print(f'quotient: {quotient_r}/{denominator} + {quotient_i}i/{denominator}')
+    print(f'remainder: {remainder_r}/{denominator}+{remainder_i}i/{denominator}')
     print(f'x ={a}+{b}i, y = {c}+{d}i')
     print(f'y = xq+r')
-    print(f'{c}+{d}i = ({a}+{b}i)({quotient_r}+{quotient_i}i)+({remainder_r}+{remainder_i}i)')
+    print(f'{c}+{d}i = ({a}+{b}i)({quotient_r}/{denominator}+{quotient_i}i/{denominator})+({remainder_r}/{denominator}+{remainder_i}i/{denominator})')
 divide_GI(1,2,3,4)
