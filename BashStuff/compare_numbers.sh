7e2dab2516a1818493b368171e1e94166ec2bf9c
CouchPotato <staung1445@gmail.com>
2023-10-30 18:45:36 +0000

    Add: shell script to compare numbers

---
+++ BashStuff/compare_numbers.sh
@@ -0,0 +1,22 @@
+read x
+read y
+
+if [ $x -gt $y ]
+
+then
+
+echo "$x is greater than $y"
+
+elif [ $x -lt $y ]
+
+then 
+
+echo "$x is less than $y"
+
+elif [ $x -eq $y ]
+
+then 
+
+echo "$x is equal to $y"
+
+fi
